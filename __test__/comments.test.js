4c91dfe476e5feb40f81a93844ae7a0083db64e5
davidabutler92 <davidabutler92@yahoo.com>
2020-12-10 13:50:04 -0800

    setup server, setup.spl and app files

---
+++ __test__/comments.test.js
@@ -0,0 +1,10 @@
+const app = require('../lib/app');
+const fs = require('fs');
+const request = require('supertest');
+const pool = require('../lib/utils/pool');
+
+describe('comments routes', () => {
+  beforeEach(() => {
+    
+  })
+});
