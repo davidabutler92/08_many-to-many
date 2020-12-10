532f4feb2d69cefab5a5422ba5eae1307c4ead07
davidabutler92 <davidabutler92@yahoo.com>
2020-12-10 14:19:08 -0800

    add test for getting all comments

--- __test__/comments.test.js
+++ __test__/comments.test.js
@@ -2,6 +2,7 @@ const app = require('../lib/app');
 const fs = require('fs');
 const request = require('supertest');
 const pool = require('../lib/utils/pool');
+const Comment = require('../lib/models/Comment');
 
 describe('comments routes', () => {
   beforeEach(() => {
@@ -23,5 +24,19 @@ describe('comments routes', () => {
       id: '1',
       text: 'this is a comment'
     });
+  });
+
+  it('should get all comments using GET', async() => {
+    const comments = await Promise.all([
+      { text: 'first comment' },
+      { text: 'second comment' },
+      { text: 'third comment' }
+    ]).map(comment => Comment.insert(comment));
+
+    const res = await request(app)
+      .get('/api/v1/comments');
+
+    expect(res.body).toEqual(expect.arrayContaining(comments));
+    expect(res.body).toHaveLength(comments.length);
   });
 });
