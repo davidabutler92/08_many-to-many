dc9e8d5d636db8371f25c2edbbffd99b8bbb224b
davidabutler92 <davidabutler92@yahoo.com>
2020-12-10 13:57:59 -0800

    add test for creating a comment via POST

--- __test__/comments.test.js
+++ __test__/comments.test.js
@@ -5,6 +5,23 @@ const pool = require('../lib/utils/pool');
 
 describe('comments routes', () => {
   beforeEach(() => {
-    
-  })
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('creates a comment via POST', async() => {
+    const res = await request(app)
+      .post('/api/v1/comments')
+      .send({
+        text: 'this is a comment'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      text: 'this is a comment'
+    });
+  });
 });
